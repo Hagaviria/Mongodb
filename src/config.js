13c1422710afc39deaa08b2c8bda6a2bbd9fe319
Harold Gaviria Londoño <hagaviria@hotmail.com>
2023-05-03 13:45:14 -0500

    1

---
+++ src/config.js
@@ -0,0 +1,5 @@
+import { config } from "dotenv";
+config();
+
+export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://0.0.0.0:27017/imgshare";
+export const PORT = process.env.PORT || 3000;
